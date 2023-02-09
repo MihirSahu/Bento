c50afdf06aca2d4f47009952fe365ad74b1b781e
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2023-02-08 22:38:55 -0600

    Center quote and add author

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -3,7 +3,7 @@ let quoteTag = document.getElementById("quote");
 const setQuote = async () => {
     let response = await fetch("https://api.quotable.io/random");
     let quote = await response.json();
-    quoteTag.innerHTML = quote.content;
+    quoteTag.innerHTML = quote.content + "\n - " + quote.author;
 }
 
 setQuote()
