5fc7f9b12b253ad929fcb54dab71567fe08bc343
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2023-02-13 20:13:40 -0500

    Add default quote

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,9 +1,14 @@
 let quoteTag = document.getElementById("quote");
 
 const setQuote = async () => {
-    let response = await fetch("https://api.quotable.io/random");
-    let quote = await response.json();
-    quoteTag.innerHTML = quote.content + "\n - " + quote.author;
+    try {
+        let response = await fetch("https://api.quotable.io/random");
+        let quote = await response.json();
+        quoteTag.innerHTML = quote.content + "\n - " + quote.author;
+    }
+    catch(err) {
+        quoteTag.innerHTML = "In my walks, every man I meet is my superior in some way, and in that I learn from him." + "\n - " + "Ralph Waldo Emerson";
+    }
 }
 
 setQuote()
